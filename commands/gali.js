3a0be98334ac59ed3feb1e288774be2536d249a3
krish-bhanushali <bhanushali123krish@gmail.com>
2020-11-08 00:10:30 +0530

    added fun gaali

--- commands/gali.js
+++ commands/gali.js
@@ -2,6 +2,8 @@ module.exports = {
 	name: 'gali',
 	description: 'Gali Command!',
 	execute(message, args) {
-		message.channel.send(`Args ${args}`);
+		var x = Math.floor(Math.random() * 10); 
+		var galli = ['chutiya','lodu','gandu','bsdk','jhantu','bc','Jhatpadoda','chaadipadu','padmakodu','himmat']  
+		message.channel.send(`${galli[x]} ${args[0]}`);
 	},
 };
