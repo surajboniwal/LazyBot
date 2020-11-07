ec194f64106c662fa4330576dd74633bccb3dd49
krish-bhanushali <bhanushali123krish@gmail.com>
2020-11-08 00:20:20 +0530

    Meme correctt

--- commands/meme.js
+++ commands/meme.js
@@ -6,24 +6,11 @@ module.exports = {
 	name: 'meme',
 	description: 'Random programming jokes!',
 	execute(message, args) {
-        if(args[0] == 'desi'){
-            args = 'DankMeme'
-        }
-        let options = {
-            url: 'https://meme-api.herokuapp.com/gimme/'+args,
-            method: 'GET'
-        }
+      
        
-        request(options, (err, response, body) => {
-            if(!err && response.statusCode == 200){
-                const obj = JSON.parse(body);
-                message.channel.send(obj.url);
-            }
-            else{
-                message.channel.send('Achha topic bhejo yaar');
-            }
+        message.channel.send('Abb maymay khud padhlo');
             
 
-        });
+       
 	},
 };
